6e2e5d742f204004c31d62e21f4c3899fe545612
nvbinh9 <sang2k1234@gmail.com>
2022-03-11 13:26:23 +0700

    continue singleton

--- src/creational/Singleton.java
+++ src/creational/Singleton.java
@@ -1,10 +1,12 @@
 package creational;
 
+import java.util.Random;
+
 public class Singleton {
 
-    private static Singleton singleton;
+    private static Singleton singleton ;
 
-    private Singleton() {};
+    private Singleton() {}
 
     public static Singleton getInstance() {
         if (singleton == null) {
@@ -24,5 +26,9 @@ class Entry {
         Singleton singleton1 = Singleton.getInstance();
         System.out.println(singleton.toString());
         System.out.println(singleton1.toString());
+        Random random = new Random(4);
+        random.nextGaussian();
+        random.nextDouble();
+        System.out.println(random);
     }
 }
